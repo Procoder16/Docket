73980e5339e7595acf12d4cb54485fc000bee685
Procoder16 <smsoumik1608@gmail.com>
2021-05-17 22:30:07 +0530

    started with the tasks screen

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TasksScreens extends StatelessWidget {
+  const TasksScreens({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
