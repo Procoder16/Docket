a12123b6c5cb2827638b9f3c3b356295a83902a4
Procoder16 <smsoumik1608@gmail.com>
2021-05-19 02:28:42 +0530

    state of the app updated with callbacks and the task list made functional

--- lib/screens/add_listTile_screen.dart
+++ lib/screens/add_listTile_screen.dart
@@ -6,33 +6,35 @@ class AddListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xFF757575),
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20.0),
-            topRight: Radius.circular(20.0),
+      child: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
+            ),
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Add Task',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.lightBlueAccent, fontSize: 30.0),
-            ),
-            TextField(
-              autofocus: true,
-              textAlign: TextAlign.center,
-            ),
-            FlatButton(
-              onPressed: () {},
-              color: Colors.lightBlueAccent,
-              child: Text('Add'),
-            ),
-          ],
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Add Task',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.lightBlueAccent, fontSize: 30.0),
+              ),
+              TextField(
+                autofocus: true,
+                textAlign: TextAlign.center,
+              ),
+              FlatButton(
+                onPressed: () {},
+                color: Colors.lightBlueAccent,
+                child: Text('Add'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = true;
+
+  void checkBoxStateFunc(bool checkBox) {
+    setState(() {
+      isChecked = checkBox;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
         'This is a task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool newValue) {},
-      ),
+      trailing: CheckedBox(isChecked, checkBoxStateFunc),
+    );
+  }
+}
+
+class CheckedBox extends StatelessWidget {
+  final bool checkboxState;
+  final Function checkBoxStateFunc;
+  CheckedBox(this.checkboxState, this.checkBoxStateFunc);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxState,
+      onChanged: checkBoxStateFunc,
     );
   }
 }
