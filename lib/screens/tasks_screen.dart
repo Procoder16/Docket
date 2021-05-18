063b88de6d87224ca7caafbda41c791f41464c0c
Procoder16 <smsoumik1608@gmail.com>
2021-05-18 16:35:37 +0530

    made the list icon

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,6 +5,25 @@ class TasksScreens extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        padding: EdgeInsets.only(top: 60.0, left: 30.0, bottom: 30.0),
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 30.0,
+              backgroundColor: Colors.white,
+              child: Icon(
+                Icons.list,
+                size: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            Text('Todoey'),
+          ],
+        ),
+      ),
+    );
   }
 }
