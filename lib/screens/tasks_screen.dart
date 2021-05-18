1b0da1f62195cbf784d5f72ff0c75726e5d10bdc
Procoder16 <smsoumik1608@gmail.com>
2021-05-18 16:44:46 +0530

    layout of the top part done

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -10,6 +10,7 @@ class TasksScreens extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.only(top: 60.0, left: 30.0, bottom: 30.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CircleAvatar(
               radius: 30.0,
@@ -20,7 +21,20 @@ class TasksScreens extends StatelessWidget {
                 color: Colors.lightBlueAccent,
               ),
             ),
-            Text('Todoey'),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Todoey',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 50.0),
+            ),
+            Text(
+              '12 Tasks',
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+            ),
           ],
         ),
       ),
