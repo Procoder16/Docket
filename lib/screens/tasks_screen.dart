58d580e352de327bf385d07263aaaea7f36708e5
Procoder16 <smsoumik1608@gmail.com>
2021-05-23 13:21:22 +0530

    edited the task counter's size

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -63,7 +63,7 @@ class TasksScreens extends StatelessWidget {
                       '${Provider.of<TaskData>(context).taskCount}',
                       style: TextStyle(
                         color: Colors.white,
-                        fontSize: 20.0,
+                        fontSize: 22.0,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
